fd3e4e0eeb097f95dba0d186c995d7724074d6e5
ranwahle <ran.wahle@gmail.com>
2016-08-03 00:18:59 +0300

    Routes

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+let express = require('express');
 
 
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/index.html');
 });
+
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
 
 io.on('connection', function(socket){
     console.log('a user connected');
