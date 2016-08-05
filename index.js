dd23e49d1372bfcb1f6b55158ff5ec023dd7943f
ranwahle <ran.wahle@gmail.com>
2016-08-05 07:20:51 +0300

    chat, topics

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let app = require('express')();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 let express = require('express');
-
+let topics = [];
 
 
 app.get('/', function(req, res){
@@ -13,19 +13,34 @@ app.use(express.static('public'));
 app.use(express.static('node_modules'));
 
 io.on('connection', function(socket){
+    //
+
     console.log('a user connected');
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
+
+    socket.on('loadTopics', () =>{
+        console.log('load Topcis request');
+        io.emit('topics', topics);
+    })
+
+    socket.on('newTopic', topic => {
+        topics.push(topic);
+        io.emit('topics', topics);
+    } );
 
     socket.on('newUser', user => {
        console.log(user);
     io.emit('newUser', user);
     });
     socket.on('chat message', msg => {
-        console.log('message: ' + msg);
+        console.log('message: ', msg);
         io.emit('chat message', msg);
     });
+
+    socket.emit('topics', topics);
+
 });
 
 http.listen(3000, function(){
---
+++ public/1.OldAngular/services/chatService.js
@@ -0,0 +1,85 @@
+/**
+ * Created by ranwahle on 04/08/2016.
+ */
+(function(angular){
+
+    var _socket, _topics = {}, _events = {}, _messages = [];
+    var service = function(){
+         _socket = io();
+
+        var self = this;
+        _socket.on('chat message', self.messageReceived);
+        _socket.on('topics',function(topics) { self.topicsLoaded.call(self, topics);});
+        Object.defineProperty(this,'topics',{
+            get: function() {return _topics}
+        });
+
+    };
+
+    service.prototype.topicsLoaded = function(topics){
+        _topics = {};
+        topics.forEach (function( topic){
+           _topics[topic] = [];
+        });
+        this.runEvents('topicListChanged', _topics);
+    };
+
+    service.prototype.messageReceived =  function(message){
+        _messages.push(message);
+      var topic = _topics[message.topic];
+        topic.forEach(function(callback){
+           callback(message.message);
+        });
+    };
+
+    service.prototype.sendMessage = function(topicName, message){
+        _socket.emit('chat message',{topic: topicName, message:  message});
+    };
+
+    service.prototype.subscribe = function(eventName, callback){
+        if (!_events[eventName]){
+           _events[eventName] = [];
+        }
+        _events[eventName].push(callback);
+
+    };
+
+    service.prototype.subscribeToTopic = function(topic, callback){
+        if (!_topics[topic]){
+            return;
+        }
+        _topics[topic].push(callback);
+
+        var topicMessages = _messages.filter(function(message){
+            return message.topic === topic;
+        });
+
+        topicMessages.forEach(function(message){
+            callback(message.message);
+        });
+
+    };
+
+    service.prototype.runEvents = function(eventName, args){
+      if (!_events[eventName]){
+          return;
+      }
+      _events[eventName].forEach(function(callback){
+          callback(args);
+      });
+    };
+
+    service.prototype.addTopic = function(topic){
+        if (_topics[topic]){
+            return;
+        }
+        _topics[topic] = [];
+        _socket.emit('newTopic', topic);
+        this.runEvents('topicListChanged', _topics);
+    };
+
+
+
+    angular.module('chatApp').service('chatService',[service]);
+
+}(window.angular));
